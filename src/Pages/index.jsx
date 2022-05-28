010420ae852e6dd6d8067494e07432011e2ada1b
AlmeenT <tiamiyualameen@ymail.com>
2022-05-28 10:22:03 -0700

    files setup

---
+++ src/Pages/index.jsx
@@ -0,0 +1,8 @@
+export {default as Calendar} from "./Calendar";
+export {default as Client} from "./Client";
+export {default as ColorPicker} from "./ColorPicker";
+export {default as Editor} from "./Editor";
+export {default as Financial} from "./Financial";
+export {default as Orders} from "./Orders";
+export {default as Product} from "./Product"
+export {default as Team} from "./Team"
